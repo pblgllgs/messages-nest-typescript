ae0527c8c6e1a0c2e5db45937d9b804ea39ae63e
pblgllgs <pbl.gllgs@gmail.com>
2022-05-28 16:05:11 -0400

    repo y services

--- src/messages/messages.controller.ts
+++ src/messages/messages.controller.ts
@@ -1,19 +1,29 @@
 import { Body, Controller, Get, Param, Post } from '@nestjs/common';
+import { CreateMessageDto } from './dtos/create-message.dto';
+import { MessagesService } from './messages.service';
 
 @Controller('/messages')
 export class MessagesController {
 
+    messagesService: MessagesService;
+
+    constructor() {
+        this.messagesService =  new MessagesService();
+    }
+
     @Get()
     listMessages() {
+        return this.messagesService.findAll();
     }
 
     @Post()
-    createMessage(@Body() body: any) {
-        console.log(body)
+    createMessage(@Body() body: CreateMessageDto) {
+        return this.messagesService.create(body.content);
     }
 
     @Get('/:id')
     getMessage(@Param('id') id: string) {
+        return this.messagesService.findOne(id);
     }
 
 }
