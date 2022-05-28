64db479aeee8ed925b7338ae7ff005169018283c
pblgllgs <pbl.gllgs@gmail.com>
2022-05-27 21:43:00 -0400

    curso nestjs

--- src/main.ts
+++ src/main.ts
@@ -1,13 +1,5 @@
 import { NestFactory } from '@nestjs/core';
-import { Message } from './interfaces/interfaces';
 import { MessagesModule } from './messages/messages.module';
-
-export const messages:Message[] = [
-  { 'content': 'mensaje 1','id': '1' },
-  { 'content': 'mensaje 2','id': '2' },
-  { 'content': 'mensaje 3','id': '3' },
-  { 'content': 'mensaje 4','id': '4' },
-]
 
 
 async function bootstrap() {
--- src/messages/messages.controller.ts
+++ src/messages/messages.controller.ts
@@ -1,55 +1,19 @@
-import { Body, Controller, Delete, Get, Param, Post, Put } from '@nestjs/common';
-import { Message } from 'src/interfaces/interfaces';
-import { messages } from 'src/main';
+import { Body, Controller, Get, Param, Post } from '@nestjs/common';
 
 @Controller('/messages')
 export class MessagesController {
 
     @Get()
-    listMessages(): Message[] {
-        return messages
+    listMessages() {
     }
 
     @Post()
-    createMessage(@Body() body: Message) {
-        const message = {
-            content: body.content,
-            id: body.id,
-        }
-        messages.push(message)
-        return message
+    createMessage(@Body() body: any) {
+        console.log(body)
     }
 
     @Get('/:id')
-    getMessage(@Param('id') id: string): Message {
-        const message = messages.find(message => message.id === id)
-        return message
+    getMessage(@Param('id') id: string) {
     }
 
-    @Delete('/:id')
-    deleteMessage(@Param('id') id: string) {
-        const message = messages.find(message => message.id === id)
-        if (!message) {
-            return {
-                message:
-                    'deleted'
-            }
-        }
-        messages.splice(messages.indexOf(message), 1)
-        return {
-            message: 'Mensaje ' + id + ' borrado'
-        }
-    }
-
-    @Put('/:id')
-    updateMessage(@Param('id') id: string, @Body() body: Message) {
-        const message = messages.find(message => message.id === id)
-        if (!message) {
-            return {
-                message: 'not found'
-            }
-        }
-        message.content = body.content
-        return message
-    }
 }
